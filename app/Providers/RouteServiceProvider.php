6bd9cd5168fdf01b05a311316e51b0633b77a07d
Your Name <you@example.com>
2024-01-30 16:26:32 +0100

    .

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,4 +47,6 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
     }
+
+    public const HOME = '/dashboard';
 }
